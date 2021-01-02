4d786deb6a07b23f429b50d61025614c197a4f28
Dean Politis <dionsspolitis@gmail.com>
2021-01-02 23:27:05 +0200

    Project initialized. Created plugin folder in the suitable Wordpress directory. Created plugin php file which holds our header comment(needed for wp plugin creation) as well as the layout php file which contains the bare minimum HTML that draws our table on the DOM.

---
+++ main_handler.php
@@ -0,0 +1,43 @@
+<?php
+    // Header comment - Used to specify plugin metadata name, author, version, licence, etc.
+    // At a minimum, a header comment must contain the plugin name.
+    // Only one file in the plugin's folder should have the header comment.
+    // If the plugin has multiple PHP files, only one of these should have the header comment.
+
+    /** 
+     * Plugin Name: Review List Plugin
+     * Plugin URI: https://github.com/DeanPol?tab=repositories
+     * Description: Presents the review data fetched from our JSON response in a tabular fashion.
+     * Author: Dean Politis
+    */
+
+    /* Actions are the hooks that the WP core launches at specific points
+    during execution, or when specific events occur. */
+
+    // First, create a callback function which will run when the action it is hooked to will run.
+    // Second, add your callback function to the action (hooking)
+    // At a minimum, the add_action() function requires two parameters:
+    // 1. the name of the action you're hooking to and
+    // 2. the name of your callback function.
+    add_action('admin_menu', 'CreatePage'); //When 'admin_menu' is called, run 'createPage'.
+
+    function CreatePage()
+    {
+
+
+        add_menu_page(
+            'Review Table', // Text displayed in the title tags of the page.
+            'Review Table', // Plugin Title text that appears on the submenu.
+            'manage_options', //Capability required for this menu to be displayed to the user.
+            'Review_Table', //visible title on URL
+            'MyCallbackFunction' //our callback function.
+        );
+    }
+
+    // Define our callback function.
+    // Using this to define which script is called.
+    function MyCallbackFunction()
+    {
+        include_once('page_layout.php');
+    }
+?>
---
+++ page_layout.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Review List Plugin</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div class="container">
+            <table class="styled-table">
+                <thead>
+                    <tr>
+                        <th>Casino</th>
+                        <th>Bonus</th>
+                        <th>Features</th>
+                        <th>Play</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div id="Image"> <img src=""> </div>
+                            <div><a href="">Review</a></div>
+                        </td>
+                        <td>
+                            <div id="Rating">RATINGIMAGE1</div>
+                            <div>BONUS</div>
+                        </td>
+                        <td>
+                            <div id="FeaturesList">
+                                <ul>
+                                    <li>Feature_1</li>
+                                    <li>Feature_2</li>
+                                    <li>Feature_3</li>
+                                </ul>
+                            </div>
+                        </td>
+                        <td>
+                            <div><a href="" class="PlayButton">Play Now</a></div>
+                            <div id="TermsNConditions">TermsNConditions</div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
